1bc05580b3528860a4ddbbfa47c9f2e71cdc42d7
Kiaze Wgna <kaizewang9494@gmail.com>
2025-12-23 22:57:10 -0500

    not working physics engine for some reason

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ import {OrbitControls} from "jsm/controls/OrbitControls.js"
 
 //constants
 const locked=false;
+var physics=false
+const tankWidth=3.7
 const chassisBoxLength=690
 const chassisBoxWidth=463
 const chassisBoxHeight=195
+const pixelPerMeter=chassisBoxWidth/tankWidth
+const tankLength=chassisBoxLength/pixelPerMeter
+const tankHeight=chassisBoxHeight/pixelPerMeter
+const playerMass=60000
+const physicsSettings=[0.4,0.2,9.8]
+const timescale=0.5
+const outBoundRate=100
 //classes
 class InputHandler {
     constructor(game){
@@ -35,6 +44,9 @@ class InputHandler {
             }
             if ((e.key === "Shift")&&(this.game.keySneak==false)){
                 this.game.keySneak=true;
+            }
+            if ((e.key === "p")||(e.key === "P")){
+                physics=true;
             }
         });
         window.addEventListener("keyup", e => {
@@ -99,8 +111,8 @@ class Terrain{
     heightAt(x, z) {
         let y = 0;
     
-        let amplitude = 300;
-        let frequency = 0.0003;
+        let amplitude = 5*pixelPerMeter;
+        let frequency = 1/(50*pixelPerMeter);
     
         for (let i = 0; i < 5; i++) {
             const nx = x * frequency;
@@ -118,13 +130,21 @@ class Terrain{
 }
 
 class Player{
-    constructor(game){
+    constructor(game,mass,length,width,height){
         this.game=game;
+        this.mass=mass
         // Chassis
-        this.chassis= new THREE.Object3D();
-        this.game.scene.add(this.chassis)
+        this.chassisL=length
+        this.chassisW=width
+        this.chassisH=height
+        this.objL=this.chassisL/this.game.pixelPerMeter
+        this.objW=this.chassisW/this.game.pixelPerMeter
+        this.objH=this.chassisH/this.game.pixelPerMeter
+        this.obj= new THREE.Object3D();
+        this.obj.position.set(0,700,0)
+        this.game.scene.add(this.obj)
         // Collision
-        this.collisionBox= new THREE.BoxGeometry(chassisBoxWidth,chassisBoxHeight,chassisBoxLength)
+        this.collisionBox= new THREE.BoxGeometry(this.chassisW,this.chassisH,this.chassisL)
         this.collisionMat= new THREE.MeshStandardMaterial({
             color: 0xcffffff,
             flatShading:true
@@ -132,11 +152,11 @@ class Player{
         this.collision= new THREE.Mesh(this.collisionBox,this.collisionMat);
         this.collision.position.y=chassisBoxHeight/2
         this.collision.position.z=-6
-        this.chassis.add(this.collision)
+        this.obj.add(this.collision)
         // Turret Pivot
         this.turretPivot= new THREE.Object3D();
         this.turretPivot.position.set(0, 210, 39)
-        this.chassis.add(this.turretPivot)
+        this.obj.add(this.turretPivot)
         // Turret
         this.turret= new THREE.Object3D();
         this.turretPivot.add(this.turret);
@@ -158,6 +178,7 @@ class Player{
         this.cameraArm.position.set(this.cameraXi, this.cameraYi, this.cameraZi); 
         this.cameraArm.rotation.y=Math.PI;
         this.cameraPivot.add(this.cameraArm);
+        this.CObject=new CObject(this,this.game.terrain,physicsSettings)
         if (locked){
             this.cameraArm.add(this.game.camera.camera)
         }
@@ -165,7 +186,7 @@ class Player{
     }
     loadModels(){
         this.game.loader.load("/assets/chassis.glb", (gltf) => {
-            this.chassis.add(gltf.scene);
+            this.obj.add(gltf.scene);
         });
         this.game.loader.load("/assets/turret.glb", (gltf) => {
             this.turret.add(gltf.scene);
@@ -176,25 +197,215 @@ class Player{
     }
     update(){
         if (this.game.keyFwd){
-            this.chassis.translateZ(1);
+            this.obj.position.z+=1;
         }
         if (this.game.keyBwd){
-            this.chassis.translateZ(-1);
+            this.obj.position.z-=1;
         }
         if (this.game.keyRight){
-            this.chassis.rotation.y-=0.3;
+            this.obj.rotation.z+=0.3;
         }
         if (this.game.keyLeft){
-            this.chassis.rotation.y+=0.3;
+            this.obj.rotation.z-=0.3;
         }
         if (this.game.keyJump){
-            this.chassis.position.y+=3;
+            this.obj.rotation.x-=0.1;
         }
         if (this.game.keySneak){
-            this.chassis.rotation.x+=0.1;
+            this.obj.rotation.x+=0.1;
+        }
+        if (physics){
+        this.CObject.update()
         }
     }
 };
+
+class CObject{
+    constructor(object,terrain,settings){
+        this.cosf=settings[0]
+        this.codf=settings[1]
+        this.g=settings[2]
+        this.accuracy=1
+        this.object=object
+        this.terrain=terrain
+        this.pixelPerMeter=this.object.game.pixelPerMeter
+        
+        this.touFR=false
+        this.touFL=false
+        this.touBR=false
+        this.touBL=false
+        
+        this.forces=[]
+        this.acceleration=[]
+        this.velocityX=0
+        this.velocityY=0
+        this.velocityZ=0
+
+        this.torqueX=0
+        this.torqueY=0
+        this.torqueZ=0
+        this.angAccX=0
+        this.angAccY=0
+        this.angAccZ=0
+        this.inertiaX = this.object.mass * ( (this.object.objL*this.object.objL)/12 + (this.object.objH*this.object.objH)/3 );
+        this.inertiaY = this.object.mass * ( (this.object.objW*this.object.objW + this.object.objL*this.object.objL) / 12 );
+        this.inertiaZ = this.object.mass * ( (this.object.objW*this.object.objW)/12 + (this.object.objH*this.object.objH)/3 );
+        this.angVecX=0
+        this.angVecY=0
+        this.angVecZ=0
+
+        this.resetForces()
+    }
+    update(){
+        this.resetForces()
+        this.getNormals()
+        this.sumForces()
+    }
+    resetForces(){
+        this.forcesFR=[0,0,0]
+        this.forcesFL=[0,0,0]
+        this.forcesBR=[0,0,0]
+        this.forcesBL=[0,0,0]
+
+        this.normalFR=[0,0,0]
+        this.normalFL=[0,0,0]
+        this.normalBR=[0,0,0]
+        this.normalBL=[0,0,0]
+
+        this.pFR = new THREE.Vector3();
+        this.pFL = new THREE.Vector3();
+        this.pBR = new THREE.Vector3();
+        this.pBL = new THREE.Vector3();
+
+        this.updateCorners()
+    }
+    updateCorners() {
+        var hx = this.object.chassisW / 2;
+        var hz = this.object.chassisL  / 2;
+
+        var euler = new THREE.Euler(this.object.obj.rotation.x, this.object.obj.rotation.y, this.object.obj.rotation.z, 'XYZ');
+
+        var FR = new THREE.Vector3( hx, 0,  hz);
+        var FL = new THREE.Vector3(-hx, 0,  hz);
+        var BR = new THREE.Vector3( hx, 0, -hz);
+        var BL = new THREE.Vector3(-hx, 0, -hz);
+
+        this.pFR.copy(FR).applyEuler(euler).add(this.object.obj.position);
+        this.pFL.copy(FL).applyEuler(euler).add(this.object.obj.position);
+        this.pBR.copy(BR).applyEuler(euler).add(this.object.obj.position);
+        this.pBL.copy(BL).applyEuler(euler).add(this.object.obj.position);
+    }
+    getNormals(){
+        var hx = this.object.objL / 2;
+        var hz = this.object.objW  / 2;
+
+        var dx = Math.sin(this.object.obj.rotation.z) * hx;
+        var dz = Math.sin(this.object.obj.rotation.x) * hz;
+    
+        var u = dx / this.object.objL + 0.5;
+        var v = dz / this.object.objW  + 0.5;
+    
+        var wFR = u * v;
+        var wFL = (1 - u) * v;
+        var wBL = (1 - u) * (1 - v);
+        var wBR = u * (1 - v);
+        var wTot=0
+
+        if (this.pFR.y<=this.terrain.heightAt(this.pFR.x,this.pFR.z)){
+            wTot+=wFR
+            this.touFR=true
+        }
+        if (this.pFL.y<=this.terrain.heightAt(this.pFL.x,this.pFL.z)){
+            wTot+=wFL
+            this.touFL=true
+        }
+        if (this.pBR.y<=this.terrain.heightAt(this.pBR.x,this.pBR.z)){
+            wTot+=wBR
+            this.touBR=true
+        }
+        if (this.pBL.y<=this.terrain.heightAt(this.pBL.x,this.pBL.z)){
+            wTot+=wBL
+            this.touBL=true
+        }
+
+        this.weightFR= this.object.mass * this.g * (wFR/wTot) + (this.terrain.heightAt(this.pFR.x,this.pFR.z)-this.pFR.y)*outBoundRate
+        this.weightFL= this.object.mass * this.g * (wFL/wTot) + (this.terrain.heightAt(this.pFL.x,this.pFL.z)-this.pFL.y)*outBoundRate
+        this.weightBR= this.object.mass * this.g * (wBR/wTot) + (this.terrain.heightAt(this.pBR.x,this.pBR.z)-this.pBR.y)*outBoundRate
+        this.weightBL= this.object.mass * this.g * (wBL/wTot) + (this.terrain.heightAt(this.pBL.x,this.pBL.z)-this.pBL.y)*outBoundRate
+    
+    
+        if (this.touFR){
+            this.touFR=false
+            var dydx=((this.terrain.heightAt(this.pFR.x+this.accuracy,this.pFR.z)-this.terrain.heightAt(this.pFR.x-this.accuracy,this.pFR.z))/(2*this.accuracy))/this.pixelPerMeter
+            var dydz=((this.terrain.heightAt(this.pFR.x,this.pFR.z+this.accuracy)-this.terrain.heightAt(this.pFR.x,this.pFR.z-this.accuracy))/(2*this.accuracy))/this.pixelPerMeter
+            this.forcesFR[0]-=dydx*this.weightFR
+            this.normalFR[0]= -dydx*this.weightFR
+            this.forcesFR[1]+=this.weightFR
+            this.normalFR[1]=this.weightFR
+            this.forcesFR[2]-=dydz*this.weightFR
+            this.normalFR[2]= -dydz*this.weightFR
+        }
+        if (this.touFL){
+            this.touFL=false
+            var dydx=((this.terrain.heightAt(this.pFL.x+this.accuracy,this.pFL.z)-this.terrain.heightAt(this.pFL.x-this.accuracy,this.pFL.z))/(2*this.accuracy))/this.pixelPerMeter
+            var dydz=((this.terrain.heightAt(this.pFL.x,this.pFL.z+this.accuracy)-this.terrain.heightAt(this.pFL.x,this.pFL.z-this.accuracy))/(2*this.accuracy))/this.pixelPerMeter
+            this.forcesFL[0]-=dydx*this.weightFL
+            this.normalFL[0]= -dydx*this.weightFL
+            this.forcesFL[1]+=this.weightFL
+            this.normalFL[1]=this.weightFL
+            this.forcesFL[2]-=dydz*this.weightFL
+            this.normalFL[2]= -dydz*this.weightFL
+        }
+        if (this.touBR){
+            this.touBR=false
+            var dydx=((this.terrain.heightAt(this.pBR.x+this.accuracy,this.pBR.z)-this.terrain.heightAt(this.pBR.x-this.accuracy,this.pBR.z))/(2*this.accuracy))/this.pixelPerMeter
+            var dydz=((this.terrain.heightAt(this.pBR.x,this.pBR.z+this.accuracy)-this.terrain.heightAt(this.pBR.x,this.pBR.z-this.accuracy))/(2*this.accuracy))/this.pixelPerMeter
+            this.forcesBR[0]-=dydx*this.weightBR
+            this.normalBR[0]= -dydx*this.weightBR
+            this.forcesBR[1]+=this.weightBR
+            this.normalBR[1]=this.weightBR
+            this.forcesBR[2]-=dydz*this.weightBR
+            this.normalBR[2]= -dydz*this.weightBR
+        }
+        if (this.touBL){
+            this.touBL=false
+            var dydx=((this.terrain.heightAt(this.pBL.x+this.accuracy,this.pBL.z)-this.terrain.heightAt(this.pBL.x-this.accuracy,this.pBL.z))/(2*this.accuracy))/this.pixelPerMeter
+            var dydz=((this.terrain.heightAt(this.pBL.x,this.pBL.z+this.accuracy)-this.terrain.heightAt(this.pBL.x,this.pBL.z-this.accuracy))/(2*this.accuracy))/this.pixelPerMeter
+            this.forcesBL[0]-=dydx*this.weightBL
+            this.normalBL[0]= -dydx*this.weightBL
+            this.forcesBL[1]+=this.weightBL
+            this.normalBL[1]=this.weightBL
+            this.forcesBL[2]-=dydz*this.weightBL
+            this.normalBL[2]= -dydz*this.weightBL
+        }
+    }
+    sumForces(){
+        this.forces=[
+            this.forcesFR[0]+this.forcesFL[0]+this.forcesBR[0]+this.forcesBL[0],
+            this.forcesFR[1]+this.forcesFL[1]+this.forcesBR[1]+this.forcesBL[1]-(this.object.mass * this.g),
+            this.forcesFR[2]+this.forcesFL[2]+this.forcesBR[2]+this.forcesBL[2]]
+        this.acceleration=[this.forces[0]/this.object.mass,this.forces[1]/this.object.mass,this.forces[2]/this.object.mass]
+        this.velocityX+= this.acceleration[0]*this.object.game.time
+        this.velocityY+= this.acceleration[1]*this.object.game.time
+        this.velocityZ+= this.acceleration[2]*this.object.game.time
+        this.object.obj.position.x+=this.velocityX*this.object.game.time*this.pixelPerMeter
+        this.object.obj.position.y+=this.velocityY*this.object.game.time*this.pixelPerMeter
+        this.object.obj.position.z+=this.velocityZ*this.object.game.time*this.pixelPerMeter
+        
+        this.torqueX=(this.forcesBR[1]+this.forcesBL[1]-this.forcesFR[1]-this.forcesFL[1])*(this.object.objL/2)
+        this.torqueY=(((this.forcesFR[0]+this.forcesFL[0]-this.forcesBR[0]-this.forcesBL[0])*(this.object.objL/2))-((this.forcesFR[2]+this.forcesBR[2]-this.forcesFL[2]-this.forcesBL[2])*(this.object.objW/2)))
+        this.torqueZ=(this.forcesFR[1]+this.forcesBR[1]-this.forcesFL[1]-this.forcesBL[1])*(this.object.objW/2)
+        this.angAccX=this.torqueX/this.inertiaX
+        this.angAccY=this.torqueY/this.inertiaY
+        this.angAccZ=this.torqueZ/this.inertiaZ
+        this.angVecX+=this.angAccX*this.object.game.time
+        this.angVecY+=this.angAccY*this.object.game.time
+        this.angVecZ+=this.angAccZ*this.object.game.time
+        this.object.obj.rotation.x+=this.angVecX*this.object.game.time
+        this.object.obj.rotation.y+=this.angVecY*this.object.game.time
+        this.object.obj.rotation.z+=this.angVecZ*this.object.game.time
+    }
+}
 
 class Camera{
     constructor(game){
@@ -250,7 +461,13 @@ class Camera{
 };
 
 class Game{
-    constructor(){
+    constructor(pixelPerMeter){
+        document.addEventListener("visibilitychange", () => {
+            if (document.hidden) {
+                this.hiddenLastTime = performance.now();
+            }
+        });        
+        this.pixelPerMeter=pixelPerMeter
         // Renderer
         this.width= window.innerWidth;
         this.height= window.innerHeight;
@@ -261,6 +478,10 @@ class Game{
         this.scene = new THREE.Scene();
         this.light = new THREE.HemisphereLight(0xffffff,0x111111,1.2);
         this.scene.add(this.light);
+        // time
+        this.last_time=performance.now();
+        this.current_time=performance.now();
+        this.time=0;
         // Game Keys
         this.input=new InputHandler(this);
         this.keyFwd=false;
@@ -287,7 +508,7 @@ class Game{
                 this.pmrem.dispose();
             });
         // Player
-        this.player=new Player(this);
+        this.player=new Player(this,playerMass,chassisBoxLength,chassisBoxWidth,chassisBoxHeight);
         this.test=new Block(this);
     }
     resize(newWidth, newHeight){
@@ -298,6 +519,13 @@ class Game{
         this.renderer.setSize(this.width, this.height);
     }
     update(){
+        this.current_time=performance.now()
+        if (this.hiddenLastTime!=0){
+            this.last_time=this.hiddenLastTime
+            this.hiddenLastTime=0
+        }
+        this.time=((this.current_time-this.last_time)/1000)*timescale
+        this.last_time=this.current_time
         this.player.update();
         this.camera.update();
     }
@@ -307,7 +535,7 @@ class Game{
 };
 
 //actual code
-var gamelis=[new Game()]
+var gamelis=[new Game(pixelPerMeter)]
 function animation() {
     gamelis[0].update();
     gamelis[0].render();
